428914564ca2acdd07fb54e155659bb0cdbf5f59
Karthik-357 <karthikcbe357@gmail.com>
2025-09-19 22:50:41 +0530

    CRITICAL: Fix ES6 export to CommonJS module.exports for Vercel compatibility

--- backend/api/register.js
+++ backend/api/register.js
@@ -28,7 +28,7 @@ const connectDB = async () => {
   }
 };
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
